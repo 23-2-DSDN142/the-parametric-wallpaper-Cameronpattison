e3510096f97c632c60d51fc934a4a7176107bcfa
Cameronpattison <140777166+Cameronpattison@users.noreply.github.com>
2023-07-31 17:46:19 +1200

    Testing

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,10 +1,10 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+let rect_width  = 40;
+let rect_height = 30;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
@@ -15,9 +15,22 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(240,240, 255); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  //angleMode(RADIANS)
+  stroke(0)
+
+  fill(255, 142, 36)
+  ellipse(100, 100, 120, 30);
+  ellipse(100, 100, 30, 120);
+ 
+ 
+
+  fill(255, 255, 0);
+  ellipse(100, 100, 60, 60);
+
+  arc(100, 100, 50, 30, 0, 180);
+
 }
